a01b315130e8d62cc7a6cdc53717e212572c873b
NicholasVieira <120346578+NicholasVieira@users.noreply.github.com>
2023-05-08 17:44:35 -0400

    Create Node.java
    This it the node class, which is staple to the code being able to actually run. This will help establish order to the queue, which from there will be the basis to implement the following code

---
+++ Node.java
@@ -0,0 +1,72 @@
+//////////////////////////////////
+//Name: Nicholas Vieira
+//Date: 5/8/2023
+//Professor: Professor Cates
+//Class: Analysis of Algorithms
+/////////////////////////////////
+
+public class Node<E> 
+{
+	private Node<E> next;
+    private E element;
+    
+    /**
+     * Creates an empty node.
+     */
+    public Node()
+    {
+        next = null;
+        element = null;
+    }
+    
+    /**
+     * Creates a node storing the specified element.
+     *
+     * @param elem the element to be stored within the new node
+     */
+    public Node(E elem)
+    {
+        next = null;
+        element = elem;
+    }
+    
+    /**
+     * Returns the node that follows this one.
+     *
+     * @return the node that follows the current one
+     */
+    public Node<E> getNext()
+    {
+        return next;
+    }
+    
+    /**
+     * Sets the node that follows this one.
+     *
+     * @param node the node to be set to follow the current one
+     */
+    public void setNext(Node<E> node)
+    {
+        next = node;
+    }
+    
+    /**
+     * Returns the element stored in this node.
+     *
+     * @return the element stored in this node
+     */
+    public E getElement()
+    {
+        return element;
+    }
+    
+    /**
+     * Sets the element stored in this node.
+     *
+     * @param elem the element to be stored in this node
+     */
+    public void setElement(E elem)
+    {
+        element = elem;
+    }
+}
