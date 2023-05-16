92b8ced618e0971d7e9325943c48f675fad26450
NicholasVieira <120346578+NicholasVieira@users.noreply.github.com>
2023-05-16 16:20:36 -0400

    Update Algorithm6_2.java
    The final version of 6.2

--- Algorithm6_2.java
+++ Algorithm6_2.java
@@ -1,65 +1,88 @@
-/* Author: Donovan Dunning
+/* Author: Donovan Dunning and Nicholas Vieira
  * Class:  CONSC.340
  * Project: 4
  * Date: 5/8/2023
  */
 
-
 import java.util.PriorityQueue;
-import java.util.Scanner;
 
-public class Algorithm6_2 {
-   static int[] maxProfit = new int[1];
+public class Algorithm6_2
+{
+	//Variables needed to test
+	static int n = 4;
+	static int W = 16;
+	static int[] p = {0, 40, 30, 50, 10};	//Arrays padded to compensate for book starting at 1
+	static int[] w = {0, 2, 5, 10, 5};
+    static int maxProfit;
+    static boolean[] bestSet = new boolean[n+1];
+    static int numOfNode = 0;
     
-    class Node implements Comparable<Node> {
-        int level;
-        int profit;
-        int weight;
-        float bound;
 
-        @Override
-        public int compareTo(Node other) {
-            return Float.compare(other.bound, this.bound);
-        }
-    }
 
-    public void knapsack3(int n, int[] p, int[] w, int W) {
-        PriorityQueue<Node> PQ = new PriorityQueue<>();
+    static void knapsack3() 
+    {
+        PriorityQueue<Node> PQ = new PriorityQueue<Node>();
         Node u, v;
 
         v = new Node();
+        numOfNode++;
         v.level = 0;
         v.profit = 0;
         v.weight = 0;
-        v.bound = bound(v, n, p, w, W);
+        v.included = new boolean[n+1];
+        v.bound = bound(v);
         PQ.add(v);
 
-        while (!PQ.isEmpty()) {
+        while (!PQ.isEmpty()) 
+        {
             v = PQ.poll();
-            if (v.bound > maxprofit[0]) {
+            if (v.bound > maxProfit) {
                 u = new Node();
+                numOfNode++;
                 u.level = v.level + 1;
                 u.weight = v.weight + w[u.level];
                 u.profit = v.profit + p[u.level];
+                u.included = new boolean[n+1];
+                
+                for(int i = 0; i <= n; i++) 
+                {
+                	u.included[i] = v.included[i];
+                }
+                u.included[u.level] = true;
 
-                if (u.weight <= W && u.profit > maxprofit[0])
-                    maxprofit[0] = u.profit;
+                if (u.weight <= W && u.profit > maxProfit)
+                    maxProfit = u.profit;
+                	for(int t = 0; t<= n; t++) 
+                	{
+                		bestSet[t] = u.included[t];
+                	}
 
-                u.bound = bound(u, n, p, w, W);
-                if (u.bound > maxprofit[0])
+                u.bound = bound(u);
+                
+                if (u.bound > maxProfit)
                     PQ.add(u);
                 
                 u = new Node();
+                numOfNode++;
                 u.weight = v.weight;
                 u.profit = v.profit;
-                u.bound = bound(u, n, p, w, W);
-                if (u.bound > maxprofit[0])
+                u.bound = bound(u);
+                u.included = new boolean[n+1];
+                
+                for(int i = 0; i <= n; i++) 
+                {
+                	u.included[i] = v.included[i];
+                }
+                u.included[u.level] = false;
+                
+                if (u.bound > maxProfit)
                     PQ.add(u);
             }
         }
     }
 
-    public float bound(Node u, int n, int[] p, int[] w, int W) {
+    public static float bound(Node u) 
+    {
         int j, k;
         int totweight;
         float result;
@@ -71,14 +94,15 @@ public class Algorithm6_2 {
         j = u.level + 1;
         totweight = u.weight;
 
-        while (j < n && totweight + w[j] <= W) {
+        while (j <= n && totweight + w[j] <= W) 
+        {
             totweight += w[j];
             result += p[j];
             j++;
         }
 
         k = j;
-        if (k < n)
+        if (k <= n)
             result += (W - totweight) * ((float) p[k] / w[k]);
 
         return result;
@@ -95,45 +119,39 @@ public class Algorithm6_2 {
         System.out.println("Max Profit: " + maxProfit[0]);
     }
     */
-    public static void main(String[] args){
-    	Algorithm6_2 algorithm = new Algorithm6_2();
-    	int n, W;
-        int[] w = new int[4];
-        int[] p = new int[4];
-        System.out.println("Enter set number out of 1-5: ");
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        //switch case inputting which set number for which test case
-        algorithm.knapsack3(n, p, w, W, maxProfit);
-        switch(input){
-           case 1: W = 16;
-                   p = {40, 30, 50, 10};
-                   w = {2, 5, 10, 5};
-                   break;
-           case 2: W = 18;
-                   p = {40, 30, 50,10};
-                   w = {2, 5, 10, 5};
-                   break;
-           case 3: W = 25;
-                   p = {50, 55, 15, 50};
-                   w = {2, 10, 5, 20};
-                   break;
-           case 4: W = 40;
-                   p = {50, 55, 15,50};
-                   w = {2, 10, 5, 20};
-                   break;
-           case 5: W = 1;
-                   p = {1, 1, 1, 1};
-                   w = {2, 3, 4, 5};
-                   break;
-        }
-        }
-       
-        System.out.println("There was a total profit of " + maxProfit);
-        System.out.println("The items selected were ");
-        System.out.println("Number of nodes visited was ");
-
+    public static void main(String[] args)
+    {
+      knapsack3();
+      
+      System.out.println("There was a total profit of " + maxProfit);
+      for(int t = 1; t <= n; t++) 
+  	  {
+      	if(bestSet[t]) 
+      	{
+      		System.out.println("Items: " + t);
+      	}
+      	
+  	  }
+      System.out.println("Number of nodes visited was " + numOfNode);
+           
      }
-  }
-}
+       
+    
+     }
+  
+  class Node implements Comparable<Node>
+  {
+      int level;
+      int profit;
+      int weight;
+      float bound;
+      boolean[] included;
+      
+      @Override
+      public int compareTo(Node other) {
+          return Float.compare(other.bound, this.bound);
+      }
+	}
+
+ 
 
